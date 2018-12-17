b8eab15a481c0a95fa67c7dbb4534df86f21d358
Ralph Largo <devlargs@gmail.com>
2018-12-18 06:23:15 +0800

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './modules/routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { UsersComponent } from './pages/admin/users/users.component';
+import { PostsComponent } from './pages/admin/posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    UsersComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
