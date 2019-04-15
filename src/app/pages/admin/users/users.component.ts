b019985baa6cda9e2ccab2c8e9f0a5d16d921f22
Christian Jay Paulo P. Sotto <cjayuu2212@gmail.com>
2019-04-16 07:47:14 +0800

    fetch users from API

--- src/app/pages/admin/users/users.component.ts
+++ src/app/pages/admin/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'app/services/api.service';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['id', 'name', 'username', 'email'];
+  dataSource = [];
+
+  constructor(public api: ApiService) { }
 
   ngOnInit() {
+    this.api.fetch('users').subscribe((users: any) => {
+      console.log(users);
+      this.dataSource = users;
+    });
     window.dispatchEvent(new Event('resize'));
   }
 
